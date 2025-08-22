22a81f4ca06029e440aed14fb6c6c7d69bffa58e
NMCJSSB2 <1152683838@qq.com>
2025-08-22 20:35:52 +0800

    Add files via upload

---
+++ 解锁功能.lua
@@ -0,0 +1,11 @@
+local StarterGui = game:GetService("StarterGui")
+
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, true)
+
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, true)
+
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, true)
+
+StarterGui:SetCore("ResetButtonCallback", true)
+
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Screenshot, true)
