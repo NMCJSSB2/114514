2a6cfe4c2e2d5fd3af3296944b774b88ccf2e463
NMCJSSB2 <1152683838@qq.com>
2025-08-23 12:27:25 +0800

    Add files via upload

---
+++ 虚构战场.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/NMCJSSB2/114514/refs/heads/main/%E6%88%98%E5%9C%BA%E8%87%AA%E7%9E%84.lua'))()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/NMCJSSB2/114514/refs/heads/main/%E6%8D%A2%E6%8C%A1%E9%94%81.lua'))()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/NMCJSSB2/114514/refs/heads/main/xhybts.lua'))()
