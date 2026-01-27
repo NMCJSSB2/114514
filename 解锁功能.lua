ce03409246170a4581fb594fb02e7d1a0d81cccc
NMCJSSB2 <1152683838@qq.com>
2026-01-28 05:01:01 +0800

    Update 解锁功能.lua

--- 解锁功能.lua
+++ 解锁功能.lua
@@ -1,11 +1,6 @@
 local StarterGui = game:GetService("StarterGui")
 
 StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, true)
-
 StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, true)
-
 StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, true)
-
 StarterGui:SetCore("ResetButtonCallback", true)
-
-StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Screenshot, true)
